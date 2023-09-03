b757774026ce6c8c4876606dd8928be080b5cbb0
christiandu66 <soun610@gmail.com>
2023-09-03 16:43:20 +0200

    rectification du texte homepages

--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -41,10 +41,11 @@ const HomePage = () => {
         <div className="presentationmoi">
           <h2 className="monh2">Qui je suis :</h2>
           <p>
-            Je suis actuellement étudiant en formation avec l'organisme
-            d'Openclassroom en tant que développeur web.
-            <br /> Je suis actuellement sur mon dernier projet avant
-            délibération de mon diplôme de développeur web.
+            Je viens de finir suis ma formation avec l'organisme d'Openclassroom
+            en tant que développeur web.
+            <br /> J’ai réussi tous les projets de cette formation et je suis en
+            attente de la décision du jury final pour la délibération de mon
+            diplôme de développeur web.
             <br /> Ayant travaillé essentiellement dans la logistique, je me
             suis reconverti dans la programmation, langage de développement.
             <br /> Passionné depuis mon adolescence par le langage et le
